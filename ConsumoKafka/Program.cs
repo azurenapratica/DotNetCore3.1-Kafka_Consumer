a205dc9ae9ce1c95423f854ef068f484b5a8c8cb
Renato Groffe <renato.groffe@yahoo.com.br>
2020-04-10 18:33:38 -0300

    .NET Core 3.1 + Console Application + Apache Kafka + Docker Compose + Helm + Kubernetes + Confluent.Kafka + Consumer
    .NET Core 3.1 + Console Application + Apache Kafka + Docker Compose + Helm + Kubernetes + Confluent.Kafka + Consumer

---
+++ ConsumoKafka/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+using Serilog;
+
+namespace ConsumoKafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            logger.Information("Testando o consumo de mensagens com Kafka");
+
+            if (args.Length != 2)
+            {
+                logger.Error(
+                    "Informe 2 parâmetros: " +
+                    "no primeiro o IP/porta para testes com o Kafka, " +
+                    "no segundo o Topic a ser utilizado no consumo das mensagens...");
+                return;
+            }
+
+            string bootstrapServers = args[0];
+            string nomeTopic = args[1];
+
+            logger.Information($"BootstrapServers = {bootstrapServers}");
+            logger.Information($"Topic = {nomeTopic}");
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                GroupId = $"{nomeTopic}-group-0",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+                {
+                    consumer.Subscribe(nomeTopic);
+
+                    try
+                    {
+                        while (true)
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            logger.Information(
+                                $"Mensagem lida: {cr.Message.Value}");
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        consumer.Close();
+                        logger.Warning("Cancelada a execução do Consumer...");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Exceção: {ex.GetType().FullName} | " +
+                             $"Mensagem: {ex.Message}");
+            }
+        }
+    }
+}
